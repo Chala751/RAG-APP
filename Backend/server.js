379a973fb3fd9eaedcf69a0b7c8f71c5dfe98443
Chala Temesgen <ctemesgen85@gmail.com>
2025-10-24 22:48:15 +0300

    server started

---
+++ Backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.use(cors());
+
+
+
+
+
+app.get("/", (req, res) => {
+  res.send("server is running...");
+});
+
+const PORT = process?.env?.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
