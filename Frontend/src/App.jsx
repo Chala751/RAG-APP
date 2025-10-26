baa2f92c8f7a5d61ff6274e914e32d05701e76b6
Chala Temesgen <ctemesgen85@gmail.com>
2025-10-26 16:05:03 +0300

    tailwind configured

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 function App() {
   return (
     <div>
-      
+      <h1 className="text-3xl font-bold underline">
+        Hello, Tailwind CSS with Vite and React!
+      </h1>
     </div>
   )
 }
