75c6441d6e1facaef6979c5784f5e508b6a198b4
Chala Temesgen <ctemesgen85@gmail.com>
2025-10-26 20:38:22 +0300

    verifz admin routed

--- Backend/routes/embedRoutes.js
+++ Backend/routes/embedRoutes.js
@@ -1,7 +1,10 @@
 import express from "express";
 import { uploadText } from "../controllers/embedController.js";
+import { verifyAdmin } from "../middleware/auth.js";
+
+
 const router = express.Router();
 
-router.post("/upload-text", uploadText);
+router.post("/upload-text",verifyAdmin, uploadText);
 
 export default router;
