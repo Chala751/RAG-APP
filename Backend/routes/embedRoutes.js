c742cf17a7136444c8053a963ac298a0ab5abc89
Chala Temesgen <ctemesgen85@gmail.com>
2025-10-26 21:49:38 +0300

    get data and delete routed

--- Backend/routes/embedRoutes.js
+++ Backend/routes/embedRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { uploadText } from "../controllers/embedController.js";
+import { deleteDocument, getAllDocuments, uploadText } from "../controllers/embedController.js";
 import { verifyAdmin } from "../middleware/auth.js";
 
 
 const router = express.Router();
+
+// Admin can see and delete documents
+router.get("/", verifyAdmin, getAllDocuments);
+router.delete("/:id", verifyAdmin, deleteDocument);
 
 router.post("/upload-text",verifyAdmin, uploadText);
 
