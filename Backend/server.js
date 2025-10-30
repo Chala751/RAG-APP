7c875471c53bdddb1f87eb4c1963c3c99e0d2e8f
Chala Temesgen <ctemesgen85@gmail.com>
2025-10-30 23:52:05 +0300

    cors added

--- Backend/server.js
+++ Backend/server.js
@@ -22,7 +22,9 @@ const app = express();
 app.use(express.json());
 const allowedOrigins = [
   "http://localhost:5173",          // local dev
-  "https://rag-app-omega.vercel.app",   // your deployed frontend
+  "https://rag-app-omega.vercel.app", 
+  "rag-app-git-main-chala751s-projects.vercel.app" ,
+  "rag-3tdhnma3w-chala751s-projects.vercel.app" // your deployed frontend
  
 ];
 
